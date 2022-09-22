8dd25292f17f156904c477e6f735a5a2f8201f0f
Ayush Dubey <33064931+devAyushDubey@users.noreply.github.com>
2022-09-22 10:48:07 +0530

    Time: 23 ms (81.15%), Space: 25.2 MB (99.96%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,43 +1,34 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    
-    TreeNode* createTree(vector<int>& preorder, int preStart, int preEnd, int inStart, int inEnd, map<int,int> &inMap){
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder)
+    {
+        std::vector<TreeNode*> parents;
+        int pre_idx = 0;
+        int in_idx = 0;
         
+        TreeNode dummy(-1); // placeholder
+        TreeNode** current = &(dummy.left);
         
-        if(preStart > preEnd || inStart > inEnd)
-            return NULL;
-        
-        TreeNode* root = new TreeNode(preorder[preStart]);
-        
-        int in = inMap[root->val];
-        int len = in - inStart;
-        root->left = createTree(preorder, preStart + 1, preStart + len, inStart, in - 1,inMap);
-        root->right = createTree(preorder, preStart + len + 1, preEnd, in + 1, inEnd, inMap);
-        
-        return root;
-        
-    }
-    
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        
-        map<int,int> inMap;
-        
-        for(int i=0;i<inorder.size();i++)
-            inMap[inorder[i]] = i;
-        
-        TreeNode* root = createTree(preorder, 0, preorder.size()-1, 0, inorder.size()-1,inMap);
-        
-        return root;
+        while(pre_idx < preorder.size())
+        {
+            while(pre_idx < preorder.size())
+            {
+                *current = new TreeNode(preorder[pre_idx++]);
+                parents.push_back(*current);
+                current = &(parents.back()->left);
+
+                if(parents.back()->val==inorder[in_idx])
+                    break;
+            }
+
+            while(!parents.empty() && parents.back()->val==inorder[in_idx])
+            {
+                current = &(parents.back()->right);
+                parents.pop_back();
+                ++in_idx;
+            }
+        }
+
+        return dummy.left;
     }
 };
