5cfa62013768f24777d02ebfbf367c80f55941fd
Ayush Dubey <33064931+devAyushDubey@users.noreply.github.com>
2022-10-16 20:23:14 +0530

    Time: 190 ms (90.36%), Space: 6.3 MB (90.12%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+  int beautySum(string s) {
+        int ans = 0;
+        int n = s.size();
+        for(int i=0; i<n; i++) {
+            int cnt[26] = {};
+            int max_f = INT_MIN;
+            int min_f = INT_MAX;
+            for(int j=i; j<n; j++) {
+                int ind = s[j] - 'a';
+                cnt[ind]++;
+                max_f = max(max_f, cnt[ind]);
+                min_f = cnt[ind];
+                for(int k=0; k<26; k++) {
+                    if(cnt[k] >= 1)
+                        min_f = min(min_f, cnt[k]);
+                }
+                ans += (max_f - min_f);
+            }
+        }
+        return ans;
+    }
+};
