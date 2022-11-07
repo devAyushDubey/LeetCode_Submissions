0f1f5626c64c2ecd2634de3f926271261022752e
Ayush Dubey <33064931+devAyushDubey@users.noreply.github.com>
2022-11-07 14:36:25 +0530

    Time: 2 ms (45.34%), Space: 5.8 MB (82.75%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        
+        int temp = num;
+        int pos_6 = 0;
+        int i = 1;
+        while(temp){
+            int digit = temp%10;
+            if(digit == 6){
+                pos_6 = i;
+            }
+            temp /= 10;
+            i *= 10;
+        }
+        //cout<<pos_6<<endl;
+        num -= (pos_6*6);
+        num += (pos_6*9);
+        
+        return num;
+    }
+};
