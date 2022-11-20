6279cd16f891d97cd98c9f351ef9aa1ee42f0fec
Ayush Dubey <33064931+devAyushDubey@users.noreply.github.com>
2022-11-20 20:14:24 +0530

    Time: 17 ms (11.07%), Space: 6.8 MB (49.30%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        
+        int n = s.length();
+        unordered_map<char,int> mp;
+        
+        for(int i=0;i<n;i++)
+            mp[s[i]] = i;
+        
+        int i=0;
+        vector<int> ans;
+        
+        while(i<n){
+            cout<<i<<" "<<mp[s[i]]<<endl;
+            if(mp.find(s[i]) != mp.end()){
+                if(i == mp[s[i]]){
+                    ans.push_back(1);
+                    mp.erase(s[i]);
+                    i++;
+                }
+                else{
+                    int t = mp[s[i]];
+                    //cout<<s[i]<<" "<<t<<endl;
+                    for(int j = i;j<=t;j++)
+                        t = max(t,mp[s[j]]);
+                    ans.push_back(t-i+1);
+                    mp.erase(s[i]);
+                    i = t+1;
+                }
+            }
+            else
+                i++;
+            
+        }
+        
+        return ans;
+    }
+};
